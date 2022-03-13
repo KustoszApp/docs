80333eb6c6ef8934d30a912aa7bbdeb89872226a
Mirek Długosz <mirek@mirekdlugosz.com>
2022-03-13 15:45:37 +0100

    livereload shell returns callable

--- run_livereload.py
+++ run_livereload.py
@@ -6,8 +6,8 @@ from livereload import shell
 globs = ("*.rst", "**/*.md", "*.py", "_static/*", "_templates/*")
 
 if __name__ == "__main__":
-    shell("make clean")
-    shell("make html")
+    shell("make clean")()
+    shell("make html")()
     server = Server()
     for glob in globs:
         server.watch(f"source/{glob}", shell("make html"), delay=1)
