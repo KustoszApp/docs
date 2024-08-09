43f9247928a71d38d9fa4e0f20beb74ac2b3135b
Mirek Długosz <mzalewsk@redhat.com>
2024-08-09 17:33:11 +0200

    Migrate conf.py to new ReadTheDocs build system
    ReadTheDocs is stopping to modify builds automatically. They recommend
    adding a snippet to have the same results as in the past. See
    https://about.readthedocs.com/blog/2024/07/addons-by-default/

--- source/conf.py
+++ source/conf.py
@@ -3,6 +3,8 @@
 # For a full list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+
 project = 'Kustosz'
 copyright = '2022, Mirek Długosz'
 author = 'Mirek Długosz'
@@ -19,6 +21,13 @@ html_favicon = "_static/favicon.svg"
 html_theme_options = {
         "sidebar_hide_name": True,
 }
+
+# https://about.readthedocs.com/blog/2024/07/addons-by-default/
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
 
 myst_enable_extensions = [
     "colon_fence",
